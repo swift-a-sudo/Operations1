56c8a00efed48edd5cc2a6f80555f0fb711378e3
swift-a-sudo <amoghajoshi2005@gmail.com>
2025-05-07 16:24:07 +0530

    save to main

---
+++ DivisionOperation1.java
@@ -0,0 +1,12 @@
+public class DivisionOperation1 {
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 0;
+        try {
+            int div= a/b;
+            System.out.println("Division: " + div);
+        } catch (ArithmeticException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
