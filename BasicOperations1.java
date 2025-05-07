8c9b0b3d198a4bfd67302bc71825494676623c6b
swift-a-sudo <amoghajoshi2005@gmail.com>
2025-05-07 16:18:49 +0530

    add file

---
+++ BasicOperations1.java
@@ -0,0 +1,13 @@
+public class BasicOperations1{
+    
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+        int diff = a - b;
+        int prod = a * b;
+        System.out.println("Addition: " + sum);
+        System.out.println("Subtraction: " + diff);
+        System.out.println("Multiplication: " + prod);
+    }
+}
